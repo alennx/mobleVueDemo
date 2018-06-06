4babb39cec61d5d5df5c298dff381831d4bdedbd
liuguangrui <4074006@qq.com>
2018-06-06 14:34:25 +0800

    项目内容

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+import App from './App'
+import router from './router'
+import VueScroller from 'vue-scroller'
+
+Vue.use(MuseUI);
+Vue.use(VueScroller);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+
